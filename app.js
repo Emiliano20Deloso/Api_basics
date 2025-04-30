deeece54ac722388f8ed45470bba428e7b1bb644
Emiliano20Deloso <emilianodelgadilloosorio1@outlook.com>
2025-04-29 21:59:13 -0600

    Correcto post-items

---
+++ app.js
@@ -0,0 +1,45 @@
+"use strict"
+import express from 'express'
+import path from 'path'
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(express.static('public'))
+
+let itemsCatalog = []
+let nextId = 1  // Contador para asignar IDs únicos
+
+// post-items 
+app.post('/api/items', (req, res) => {
+    const { name, type, effect } = req.body
+  
+    //Valida que lleguen todos los campos
+    if (!name || !type || !effect) {
+      return res.status(400).json({ message: "Faltan name, type o effect" })
+    }
+  
+    //Verifica duplicados por nombre
+    if (itemsCatalog.some(item => item.name === name)) {
+      return res.status(409).json({ message: "Ese item ya existe" })
+    }
+  
+    // 3) Crea el nuevo item con ID único
+    const newItem = { id: nextId++, name, type, effect }
+  
+    // 4) Guardamos en el catálogo
+    itemsCatalog.push(newItem)
+  
+    // 5) Devolverlo con código 201 Created
+    res.status(201).json(newItem)
+  })
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+
+
+
+
